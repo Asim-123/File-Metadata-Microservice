a641253c12f885abb4e97395612d28b9698e8bff
Muhammad Asim <MuhammadAsim2222@yahoo.com>
2025-07-07 11:19:06 +0500

    Fix file upload issues and add debugging

--- functions/fileanalyse.js
+++ functions/fileanalyse.js
@@ -1,6 +1,9 @@
 const busboy = require('busboy');
 
 exports.handler = (event, context, callback) => {
+  console.log('Function called with method:', event.httpMethod);
+  console.log('Headers:', JSON.stringify(event.headers));
+  
   // Handle CORS preflight requests
   if (event.httpMethod === 'OPTIONS') {
     return callback(null, {
@@ -28,25 +31,55 @@ exports.handler = (event, context, callback) => {
     });
   }
 
+  // Check if we have a body
+  if (!event.body) {
+    return callback(null, {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      },
+      body: JSON.stringify({ error: 'No request body' })
+    });
+  }
+
   // Parse multipart form data
-  const bb = busboy({ headers: event.headers });
+  const bb = busboy({ 
+    headers: event.headers,
+    limits: {
+      fileSize: 10 * 1024 * 1024 // 10MB limit
+    }
+  });
   let fileInfo = null;
 
   bb.on('file', (fieldname, file, info) => {
-    let size = 0;
-    file.on('data', (data) => {
-      size += data.length;
-    });
-    file.on('end', () => {
-      fileInfo = {
-        name: info.filename,
-        type: info.mimeType,
-        size: size
-      };
-    });
+    console.log('File field received:', fieldname);
+    console.log('File info:', JSON.stringify(info));
+    
+    // Only process the 'upfile' field
+    if (fieldname === 'upfile') {
+      let size = 0;
+      file.on('data', (data) => {
+        size += data.length;
+      });
+      file.on('end', () => {
+        fileInfo = {
+          name: info.filename,
+          type: info.mimeType,
+          size: size
+        };
+        console.log('File processed:', JSON.stringify(fileInfo));
+      });
+    } else {
+      console.log('Skipping field:', fieldname);
+      // Skip other fields
+      file.resume();
+    }
   });
 
   bb.on('finish', () => {
+    console.log('Busboy finished, fileInfo:', fileInfo);
     if (!fileInfo) {
       callback(null, {
         statusCode: 400,
@@ -55,7 +88,7 @@ exports.handler = (event, context, callback) => {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         },
-        body: JSON.stringify({ error: 'No file uploaded' })
+        body: JSON.stringify({ error: 'No file uploaded or invalid field name' })
       });
     } else {
       callback(null, {
@@ -70,5 +103,31 @@ exports.handler = (event, context, callback) => {
     }
   });
 
-  bb.end(Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8'));
+  bb.on('error', (err) => {
+    console.error('Busboy error:', err);
+    callback(null, {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      },
+      body: JSON.stringify({ error: 'Error parsing file upload: ' + err.message })
+    });
+  });
+
+  try {
+    bb.end(Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8'));
+  } catch (err) {
+    console.error('Error ending busboy:', err);
+    callback(null, {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      },
+      body: JSON.stringify({ error: 'Error processing request: ' + err.message })
+    });
+  }
 }; 
